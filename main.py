4a170b8b3781e89fbbeb86e2ef85e2bc02bfd9d6
itzsoulbrahs <baldminecraft7@gmail.com>
2026-09-22 23:16:16 +0530

    Override env with .env values so WEBHOOK_SECRET empty takes effect

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 from fastapi import FastAPI, Request, HTTPException, Query
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
